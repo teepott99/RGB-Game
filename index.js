c2bc7eb0eb1efd7cb6513fdd300fce33f7268940
Mariah Dunn <mariahruthdesign@gmail.com>
2018-05-07 18:22:48 -0400

    Div 2 is taking on random values from preselected colors. Inputs reflect current valus of div2 at beginning of game.

--- index.js
+++ index.js
@@ -1,17 +1,46 @@
 var inputColor = [
     {r: 30, g:50, b:60},
 ];
+var input = $("#input");
 
 function rgb() {
+    //grabs the values of the input boxes
     var red = $("#red").val();
     var green = $("#green").val();
     var blue = $("#blue").val();
-    
-    document.getElementById("input").style.backgroundColor =
-    "rgb(" + red + ", " + green + ", " + blue + ")";
+    //replaces div2 RGB values with those from input boxes
+    input.css("background-color", "rgb(" + red + ", " + green + ", " + blue + ")");
+    ;
 
-  }
+}
+
+function randomSelector(arr){
+    if (arr === []) {
+    return;
+    } else {
+        var random = arr[Math.floor(Math.random() * arr.length)];
+        return random;
+    }
+}
+
+// var colorValues = [];
+
+// function pushColor() {
+//     colorValues.push(randomSelector(inputColor));
+//     return colorValues;
+// }
+
+function randomInputDiv(inputColor) {
+    input.css("background-color", "rgb(" + inputColor[0].r + ", " + inputColor[0].g + ", " + inputColor[0].b + ")");
+    $("#red").val(inputColor[0].r)
+    $("#green").val(inputColor[0].g)
+    $("#blue").val(inputColor[0].b)
+    return;
+}
+
+// console.log(randomInputDiv(colorValues))
 
 $(document).ready(function(){
     rgb();
+    randomInputDiv(inputColor)
 });
