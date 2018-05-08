4eff2c309fc3789c50ecfd302c51fe4f39d73473
Mariah Dunn <mariahruthdesign@gmail.com>
2018-05-07 21:29:10 -0400

    Struggling to calculate difference between input and match

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
+//Sets random values for Input Div
 var r = Math.floor(Math.random()*256);          // Random between 0-255
 var g = Math.floor(Math.random()*256);          // Random between 0-255
 var b = Math.floor(Math.random()*256);          // Random between 0-255
 var randomRGB = 'rgb(' + r + ',' + g + ',' + b + ')'; // Collect all to a string
 
+//Sets random values for Match Div
+var rM = Math.floor(Math.random()*256);          // Random between 0-255
+var gM = Math.floor(Math.random()*256);          // Random between 0-255
+var bM = Math.floor(Math.random()*256);          // Random between 0-255
+var randomRGBMatch = 'rgb(' + rM + ',' + gM + ',' + bM + ')'; // Collect all to a string
+
 function randomInputDiv() {
-    //replaces div2 RGB values with those from input boxes
-    $("#input").css("background-color", randomRGB);
     //applies random color to input div
     $("#input").css("background-color", randomRGB);
     //applies random color values to RGB inputs
@@ -16,10 +21,8 @@ function randomInputDiv() {
 }
 
 function randomMatchDiv() {
-    //replaces div2 RGB values with those from input boxes
-    $("#match").css("background-color", randomRGB);
     //applies random color to match div
-    match.css("background-color", randomRGB);
+    $("#match").css("background-color", randomRGBMatch);
     return;
 }
 
@@ -34,12 +37,36 @@ $("#start-btn").click(function(){
       }
       if (counter === 0) {
          console.log('sorry, out of time');
+         
          clearInterval(counter);
+         calculateScore();
        }
      }, 1000);
 })
 
+//calculate score
+// function calculateRed(r, rM) {
+//     var redScore = $("#score-red").text();
+//     redScore = redScore.text(parseInt((Math.abs((r - rM) - 255)), 0));
+//     return;
+// }
+
+function calculateRed(r, rM) {
+    let redScore = $("#score-red");
+    return redScore.text(Math.abs((r - rM) - 255));
+}
+
+console.log($("#score-red"))
+
 $(document).ready(function(){
     randomInputDiv();
+    randomMatchDiv();
+    calculateRed();
+    
 });
 
+    // var greenScore = document.getElementById("score-green");
+    // var blueScore = document.getElementById("score-blue");
+    
+    // var totalScore = document.getElementById("total-score");
+    // var totalScore = redScore+greenScore+blueScore;
